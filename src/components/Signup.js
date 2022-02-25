add3959b965ddabd50de8fb6ad776f36c2519d7a
Dushani-Ranasinghe <uththaradushani@gmail.com>
2022-02-25 17:04:40 +0530

    Created signup button and styled it

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -6,6 +6,7 @@ import "../styles/signup.css"
 function Signup() {
   return (
     <div className='Signup'>
+        
         <form className='signup-form'>
             <img className="logo-img" src={Logo} alt="cresetra-logo"/>
             <h1> SIGN UP</h1>
@@ -25,6 +26,11 @@ function Signup() {
             <div className='form-group'>
                  <input type="text" className='form-control' placeholder='Confirm Password'/>
             </div>
+            <button type="submit" className="signup-btn">SIGN UP</button>
+                <p>
+                    Already Have An Account?
+                </p>
+                <a href="#">Login</a>
         </form>
     </div>
   )
