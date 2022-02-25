7f0808d0308e07441920dc747d4b5175efab0ca2
Dushani-Ranasinghe <uththaradushani@gmail.com>
2022-02-25 16:54:55 +0530

    Added background image to signup page

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -2,15 +2,28 @@ import React from 'react'
 import Logo from  "../resources/Crestera-Logo.png"
 import "../styles/signup.css"
 
+
 function Signup() {
   return (
-    <div>
+    <div className='Signup'>
         <form className='signup-form'>
             <img className="logo-img" src={Logo} alt="cresetra-logo"/>
             <h1> SIGN UP</h1>
 
             <div className='form-group'>
                  <input type="text" className='form-control' placeholder='First Name'/>
+            </div>
+            <div className='form-group'>
+                 <input type="text" className='form-control' placeholder='Last Name'/>
+            </div>
+            <div className='form-group'>
+                 <input type="text" className='form-control' placeholder='Email'/>
+            </div>
+            <div className='form-group'>
+                 <input type="text" className='form-control' placeholder='Password'/>
+            </div>
+            <div className='form-group'>
+                 <input type="text" className='form-control' placeholder='Confirm Password'/>
             </div>
         </form>
     </div>
